3e9480ac6517b5f06a108eb6ce0bc418a0fd2135
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-13 18:49:47 +0500

    onGenerateRoute

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -3,4 +3,18 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 
 class AppRouter {
+  static Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "/":
+        return MaterialPageRoute(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              title: Text("data"),
+            ),
+          ),
+        );
+      default:
+        return null;
+    }
+  }
 }
