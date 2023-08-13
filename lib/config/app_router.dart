fe71e1717f72a97cdb2f4025608f6ab60f0f92f7
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-13 19:39:25 +0500

    HomeScreen

--- lib/config/app_router.dart
+++ lib/config/app_router.dart
@@ -1,17 +1,11 @@
+import 'package:cargo/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static Route? onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
       case "/":
-        return MaterialPageRoute(
-          
-          builder: (context) => Scaffold(
-            appBar: AppBar(
-              title: const Text("data"),
-            ),
-          ),
-        );
+        return MaterialPageRoute(builder: (context) => HomeScreen());
       default:
         return null;
     }
