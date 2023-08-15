b88cce2d09372345229df7135a211692585e4e73
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-15 11:15:20 +0500

    changes

--- lib/logic/emloyee/bloc/bloc/employee_state.dart
+++ lib/logic/emloyee/bloc/bloc/employee_state.dart
@@ -1,10 +1,21 @@
-part of 'employee_bloc.dart';
+part of 'all_notifications_bloc.dart';
 
-sealed class EmployeeState extends Equatable {
-  const EmployeeState();
-  
+
+class AllNotificationsState extends Equatable {
+  const AllNotificationsState({
+    this.notifications = const [],
+  });
+
+  final List<NotificationModel> notifications;
+
+  AllNotificationsState copyWith({
+    List<NotificationModel>? notifications,
+  }) {
+    return AllNotificationsState(
+      notifications: notifications ?? this.notifications,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [notifications];
 }
-
-final class EmployeeInitial extends EmployeeState {}
