144689b4d22cb397fd9c53fbe5bf7ca3d1436fcf
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-15 10:52:09 +0500

    EmployeeScreen

---
+++ lib/presentation/screens/employee_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeeScreen extends StatefulWidget {
+  const EmployeeScreen({super.key});
+
+  @override
+  State<EmployeeScreen> createState() => _EmployeeScreenState();
+}
+
+class _EmployeeScreenState extends State<EmployeeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
