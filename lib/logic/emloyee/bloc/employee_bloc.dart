34d07a4ca08a936b330428c4a746ef1af76fa4cd
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-16 09:52:18 +0500

    changes

--- lib/logic/emloyee/bloc/employee_bloc.dart
+++ lib/logic/emloyee/bloc/employee_bloc.dart
@@ -1,9 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:cargo/logic/emloyee/model/employee_model.dart';
 
 part 'employee_event.dart';
 part 'employee_state.dart';
+
+const _postLimit = 10;
 
 class EmployeeBloc extends Bloc<EmployeeEvent, EmployeeState> {
   EmployeeBloc() : super(const EmployeeState()) {
@@ -12,6 +17,53 @@ class EmployeeBloc extends Bloc<EmployeeEvent, EmployeeState> {
 
   Future<void> _onEmployeesFetched(
       EmployeesFetched event, Emitter<EmployeeState> emitter) async {
-        if (state.)
+    if (state.hasReachedMax) return;
+
+    try {
+      if (state.status == EmployeeStatus.initial) {
+        final employees = await _fetchEmployees(page: state.page);
+        return emit(
+          state.copyWith(
+            status: EmployeeStatus.success,
+            employees: employees,
+            hasReachedMax: employees.length < _postLimit,
+          ),
+        );
       }
+
+      final employees = await _fetchEmployees(page: state.page + 1);
+      emit(
+        employees.isEmpty
+            ? state.copyWith(hasReachedMax: true)
+            : state.copyWith(
+                status: EmployeeStatus.success,
+                employees: List.of(state.employees)..addAll(employees),
+                hasReachedMax: false,
+                page: state.page + 1,
+              ),
+      );
+    } catch (e) {}
+  }
+
+  Future<List<EmployeeModel>> _fetchEmployee(
+      {required String postId, required int page}) async {
+    String? token = await getAuthToken();
+    final response = await httpClient.get(
+      getServerRoute(
+        route: '/api/v1/comments/$postId',
+        params: {
+          'page': '$page',
+          'post_id': postId,
+        },
+      ),
+      headers: <String, String>{
+        HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: token ?? '',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final body = json.decode(response.body)["data"] as List;
+    throw Exception('error fetching posts');
+  }
 }
---
+++ lib/logic/emloyee/bloc/employee_state.dart
@@ -0,0 +1,39 @@
+part of 'employee_bloc.dart';
+
+enum EmployeeStatus { initial, success, failure }
+
+class EmployeeState extends Equatable {
+  const EmployeeState({
+    this.status = EmployeeStatus.initial,
+    this.employees = const <EmployeeModel>[],
+    this.hasReachedMax = false,
+    this.page = 0,
+  });
+
+  final EmployeeStatus status;
+  final List<EmployeeModel> employees;
+  final int page;
+  final bool hasReachedMax;
+
+  EmployeeState copyWith({
+    EmployeeStatus? status,
+    List<EmployeeModel>? employees,
+    bool? hasReachedMax,
+    int? page,
+  }) {
+    return EmployeeState(
+      status: status ?? this.status,
+      employees: employees ?? this.employees,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      page: page ?? this.page,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''employeestate { status: $status, hasReachedMax: $hasReachedMax, employees: ${employees.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, employees, hasReachedMax, page];
+}
