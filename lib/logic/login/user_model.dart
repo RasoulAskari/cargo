29c0bdedd95acb010db56167cfcebb27d4087033
RasoulAskari <rasoul.merzai123@gmail.com>
2023-08-24 10:35:02 +0500

    user model constructor

--- lib/logic/login/user_model.dart
+++ lib/logic/login/user_model.dart
@@ -4,4 +4,11 @@ class UserModel extends Equatable {
   final String password;
   final String role;
   final String permission;
+
+  UserModel(
+      {required this.name,
+      required this.email,
+      required this.password,
+      required this.role,
+      required this.permission});
 }
