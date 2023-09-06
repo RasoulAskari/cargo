015d287e5bf0294966d103491aa07072e04a6023
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-06 09:17:24 +0500

    multi bloc Provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:cargo/config/app_router.dart';
+import 'package:cargo/logic/emloyee/bloc/employee_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(const MyApp());
+
+void main() => runApp( MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Cargo",
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRouter.onGenerateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<EmployeeBloc>(
+          create: (context) => EmployeeBloc(httpClient: ),
+        ),
+      ],
+      child: Container(),
     );
   }
 }
