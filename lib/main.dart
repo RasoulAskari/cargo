491652d81dbce06967f99487d4ab9c265d243162
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-06 18:14:12 +0500

    debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         onGenerateRoute: AppRouter.onGenerateRoute,
         title: "Cargo",
         home: EmployeeScreen(),
