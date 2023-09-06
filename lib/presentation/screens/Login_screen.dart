6795f48511b40be64193f10ecb543d2c78447dc5
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-06 18:27:29 +0500

    wrap with Scaffold login screen

--- lib/presentation/screens/Login_screen.dart
+++ lib/presentation/screens/Login_screen.dart
@@ -10,8 +10,10 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Login Screen"),
+    return Scaffold(
+      body: Center(
+        child: Text("Login Screen"),
+      ),
     );
   }
 }
