5cd455b1755636ab2f6a82061d1a8014e098f826
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-06 22:17:24 +0500

    colors

---
+++ lib/config/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xffff711b);
+const Color lightPrimaryColor = Color.fromRGBO(255, 113, 27, 0.09);
+const Color secondaryColor = Color(0xff202020);
