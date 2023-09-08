dd84386de79cf13463b12c5e62dfbfdff9c116bc
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-08 12:48:16 +0500

    login_model.dart

--- lib/logic/login/bloc/login_state.dart
+++ lib/logic/login/bloc/login_state.dart
@@ -1,10 +1,39 @@
 part of 'login_bloc.dart';
 
-sealed class LoginState extends Equatable {
-  const LoginState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum EmployeeStatus { initial, success, failure }
 
-final class LoginInitial extends LoginState {}
+class EmployeeState extends Equatable {
+  const EmployeeState({
+    this.status = EmployeeStatus.initial,
+    this.employees = const <EmployeeModel>[],
+    this.hasReachedMax = false,
+    this.page = 0,
+  });
+
+  final EmployeeStatus status;
+  final List<EmployeeModel> employees;
+  final int page;
+  final bool hasReachedMax;
+
+  EmployeeState copyWith({
+    EmployeeStatus? status,
+    List<EmployeeModel>? employees,
+    bool? hasReachedMax,
+    int? page,
+  }) {
+    return EmployeeState(
+      status: status ?? this.status,
+      employees: employees ?? this.employees,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      page: page ?? this.page,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''employeestate { status: $status, hasReachedMax: $hasReachedMax, employees: ${employees.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, employees, hasReachedMax, page];
+}
