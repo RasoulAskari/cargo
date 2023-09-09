5c516a2ee2ae200ad6dfeb7cedb79ba82d531345
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-09 20:05:43 +0500

    remove

--- lib/logic/login/user_model.dart
+++ lib/logic/login/user_model.dart
@@ -6,12 +6,12 @@ import 'package:equatable/equatable.dart';
 class UserModel extends Equatable {
   final String name;
   final String email;
-  final String? token;
+  final String token;
 
   UserModel({
     required this.name,
     required this.email,
-    this.token,
+    required this.token,
   });
 
   @override
@@ -21,8 +21,6 @@ class UserModel extends Equatable {
   UserModel copyWith({
     String? name,
     String? email,
-    String? role,
-    String? permission,
     String? token,
   }) {
     return UserModel(
