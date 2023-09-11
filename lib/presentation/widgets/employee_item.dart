1c1d1efa58135e28aad09130655816f7d052cc0a
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-11 16:36:10 +0500

    EmployeeItem wedget

---
+++ lib/presentation/widgets/employee_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeeItem extends StatefulWidget {
+  const EmployeeItem({super.key});
+
+  @override
+  State<EmployeeItem> createState() => _EmployeeItemState();
+}
+
+class _EmployeeItemState extends State<EmployeeItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
