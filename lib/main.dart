850bcc5c7bb068b366a67877d1f7862f57750be0
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-11 16:20:28 +0500

    delete extra code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:cargo/app.dart';
-import 'package:cargo/config/app_router.dart';
 import 'package:cargo/logic/emloyee/bloc/employee_bloc.dart';
 import 'package:cargo/logic/login/bloc/login_bloc.dart';
-import 'package:cargo/presentation/screens/employee_screen.dart';
-import 'package:cargo/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
