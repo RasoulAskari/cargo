ff66a5ba441384c94431a68c0d287a384a538525
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-11 18:58:30 +0500

    remove warnnings

--- lib/presentation/widgets/employee_item.dart
+++ lib/presentation/widgets/employee_item.dart
@@ -25,13 +25,14 @@ class _EmployeeItemState extends State<EmployeeItem> {
           ),
           margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
           child: ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
             leading: const CircleAvatar(
               backgroundImage: NetworkImage(
                   'https://media.istockphoto.com/id/1391718981/photo/portrait-of-a-confident-young-businessman-standing-with-his-arms-crossed-in-an-office.jpg?s=1024x1024&w=is&k=20&c=cqIGFJ52Gq8D7bkJg3PqbHxOdKVCQIGUVjM6JNzFY7E='),
             ),
             title: Container(
-              margin: EdgeInsets.only(bottom: 8),
+              margin: const EdgeInsets.only(bottom: 8),
               child: Text(
                   "${widget.employee.firstName} ${widget.employee.lastName}"),
             ),
@@ -39,17 +40,17 @@ class _EmployeeItemState extends State<EmployeeItem> {
                 "${widget.employee.email}\n${widget.employee.phoneNumber}"),
           ),
         ),
-        const SizedBox(height: 3),
+        const SizedBox(height: 2),
         Container(
           height: 40,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(20),
               bottomRight: Radius.circular(20),
             ),
           ),
-          margin: EdgeInsets.symmetric(horizontal: 10),
+          margin: const EdgeInsets.symmetric(horizontal: 10),
           child: Row(),
         )
       ],
