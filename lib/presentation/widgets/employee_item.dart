7d412a85051c0dccf30b274fc2cdeedb0b73d856
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-11 18:46:05 +0500

    add border to ListTile

--- lib/presentation/widgets/employee_item.dart
+++ lib/presentation/widgets/employee_item.dart
@@ -12,33 +12,35 @@ class EmployeeItem extends StatefulWidget {
 class _EmployeeItemState extends State<EmployeeItem> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(color: Colors.white),
-            margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-            child: ListTile(
-              leading: const CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://media.istockphoto.com/id/1391718981/photo/portrait-of-a-confident-young-businessman-standing-with-his-arms-crossed-in-an-office.jpg?s=1024x1024&w=is&k=20&c=cqIGFJ52Gq8D7bkJg3PqbHxOdKVCQIGUVjM6JNzFY7E='),
-              ),
-              title: Text(
-                  "${widget.employee.firstName} ${widget.employee.lastName}"),
-              subtitle: Text(
-                  "${widget.employee.email}\n${widget.employee.phoneNumber}"),
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+          margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+          child: ListTile(
+            leading: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media.istockphoto.com/id/1391718981/photo/portrait-of-a-confident-young-businessman-standing-with-his-arms-crossed-in-an-office.jpg?s=1024x1024&w=is&k=20&c=cqIGFJ52Gq8D7bkJg3PqbHxOdKVCQIGUVjM6JNzFY7E='),
             ),
+            title: Text(
+                "${widget.employee.firstName} ${widget.employee.lastName}"),
+            subtitle: Text(
+                "${widget.employee.email}\n${widget.employee.phoneNumber}"),
           ),
-          const SizedBox(height: 10),
-          Container(
-            
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              children: [Text("data")],
-            ),
-          )
-        ],
-      ),
+        ),
+        const SizedBox(height: 3),
+        Container(
+          height: 30,
+          decoration: BoxDecoration(color: Colors.white),
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [Text("data")],
+          ),
+        )
+      ],
     );
   }
 }
