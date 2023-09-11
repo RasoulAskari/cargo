e210369d9324338918ea8f0d5f9958c661cb0fdc
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-11 18:42:32 +0500

    add background color to listTile

--- lib/presentation/widgets/employee_item.dart
+++ lib/presentation/widgets/employee_item.dart
@@ -13,22 +13,25 @@ class _EmployeeItemState extends State<EmployeeItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
-      margin: const EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: [
-          ListTile(
-            leading: const CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://media.istockphoto.com/id/1391718981/photo/portrait-of-a-confident-young-businessman-standing-with-his-arms-crossed-in-an-office.jpg?s=1024x1024&w=is&k=20&c=cqIGFJ52Gq8D7bkJg3PqbHxOdKVCQIGUVjM6JNzFY7E='),
-            ),
-            title: Text(
-                "${widget.employee.firstName} ${widget.employee.lastName}"),
-            subtitle: Text(
-                "${widget.employee.email}\n${widget.employee.phoneNumber}"),
-          ),
-          SizedBox(height: 10),
           Container(
+            decoration: BoxDecoration(color: Colors.white),
+            margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: ListTile(
+              leading: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://media.istockphoto.com/id/1391718981/photo/portrait-of-a-confident-young-businessman-standing-with-his-arms-crossed-in-an-office.jpg?s=1024x1024&w=is&k=20&c=cqIGFJ52Gq8D7bkJg3PqbHxOdKVCQIGUVjM6JNzFY7E='),
+              ),
+              title: Text(
+                  "${widget.employee.firstName} ${widget.employee.lastName}"),
+              subtitle: Text(
+                  "${widget.employee.email}\n${widget.employee.phoneNumber}"),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            
             margin: EdgeInsets.symmetric(horizontal: 15),
             child: Row(
               children: [Text("data")],
