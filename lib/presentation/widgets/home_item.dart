a75c10c4268a40a7f547a7e84ae957dccab67bf8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-12 18:12:18 +0500

    HomeItem

---
+++ lib/presentation/widgets/home_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeItem extends StatefulWidget {
+  const HomeItem({super.key});
+
+  @override
+  State<HomeItem> createState() => _HomeItemState();
+}
+
+class _HomeItemState extends State<HomeItem> {
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(child: );
+  }
+}
