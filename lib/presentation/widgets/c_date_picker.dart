ff8ff26ec21835ab47078cccf49b90d611777121
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-14 16:36:43 +0500

    changes

---
+++ lib/presentation/widgets/c_date_picker.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:raavi/presentation/icons/raavi_icons.dart';
+import 'package:raavi/presentation/widgets/form/c_date_picker_bottom_sheet.dart';
+
+class CDatePicker extends StatefulWidget {
+  final String? hintText;
+  final Function setValue;
+  final DateTime? value;
+  const CDatePicker({
+    super.key,
+    this.hintText,
+    required this.setValue,
+    this.value,
+  });
+
+  @override
+  State<CDatePicker> createState() => _CDatePickerState();
+}
+
+class _CDatePickerState extends State<CDatePicker> {
+  var txt = TextEditingController();
+  DateTime? _selectedDate;
+
+  @override
+  void initState() {
+    setState(() {
+      _selectedDate = widget.value ?? DateTime.now();
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: txt,
+      readOnly: true,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.grey[200],
+        hintText: widget.hintText,
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 16,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6.0),
+          borderSide: const BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+        suffixIcon: Icon(
+          Icons.calendar_month,
+          color: Colors.grey[600],
+          size: 20,
+        ),
+      ),
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => CDatePickerBottomSheet(
+            value: _selectedDate,
+            setValue: (DateTime value) {
+              setState(() {
+                _selectedDate = value;
+              });
+              widget.setValue(_selectedDate);
+              txt.text = DateFormat.yMMMMd(
+                      Localizations.localeOf(context).toLanguageTag())
+                  .format(value);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
