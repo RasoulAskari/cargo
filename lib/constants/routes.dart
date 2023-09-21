03b039b29b2f0f91ef5c21bbe0d60db7bebf0ba5
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 17:01:06 +0500

    addIncomingOutGoing route name

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,5 +1,4 @@
 const String home = '/home';
 const String addEmployee = '/addEmployee';
 const String employeeScreen = '/employeeScreen';
-
-
+const String addIncomingOutGoing = '/addIncomingOutGoing';
