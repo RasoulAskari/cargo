25df73566504be51121e65696c109cb936304fbf
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 17:29:44 +0500

    IncomingOutGoing

--- lib/logic/incoming_out_going/model/incoming_out_going.dart
+++ lib/logic/incoming_out_going/model/incoming_out_going.dart
@@ -1,6 +1,17 @@
 import 'package:equatable/equatable.dart';
 
 class IncomingOutGoing extends Equatable {
+  final String name;
+  final String type;
+  final double amount;
+  final Map<String, dynamic> createdBy;
+
+  IncomingOutGoing(
+      {required this.name,
+      required this.type,
+      required this.amount,
+      required this.createdBy});
+
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
