b0553a1059ca3167c9a8f929f532206c8609b8f9
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 17:38:37 +0500

    IncomingOutGoing serialization

--- lib/logic/incoming_out_going/model/incoming_out_going.dart
+++ lib/logic/incoming_out_going/model/incoming_out_going.dart
@@ -1,5 +1,9 @@
-import 'package:cargo/logic/login/user_model.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
+
+import 'package:cargo/logic/login/user_model.dart';
 
 class IncomingOutGoing extends Equatable {
   final String name;
@@ -12,4 +16,27 @@ class IncomingOutGoing extends Equatable {
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'type': type,
+      'amount': amount,
+      'user': user.toMap(),
+    };
+  }
+
+  factory IncomingOutGoing.fromMap(Map<String, dynamic> map) {
+    return IncomingOutGoing(
+      map['name'] as String,
+      map['type'] as String,
+      map['amount'] as double,
+      UserModel.fromMap(map['user'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory IncomingOutGoing.fromJson(String source) =>
+      IncomingOutGoing.fromMap(json.decode(source) as Map<String, dynamic>);
 }
