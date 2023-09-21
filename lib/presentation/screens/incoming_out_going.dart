dabeb0fdcce8e9dd7b33fecb1b6635d7e4da5e4c
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 16:59:15 +0500

    IncomingOutGoing appbar

--- lib/presentation/screens/incoming_out_going.dart
+++ lib/presentation/screens/incoming_out_going.dart
@@ -10,6 +10,32 @@ class IncomingOutGoing extends StatefulWidget {
 class _IncomingOutGoingState extends State<IncomingOutGoing> {
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ))
+        ],
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Employee Screen",
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
