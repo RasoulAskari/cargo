713b67748fcc1662c376f131428f492ef9ef6321
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 18:19:40 +0500

    _fetchincoming_out_going

--- lib/logic/incoming_out_going/bloc/bloc/incoming_out_going_bloc.dart
+++ lib/logic/incoming_out_going/bloc/bloc/incoming_out_going_bloc.dart
@@ -1,14 +1,71 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
+import 'package:cargo/logic/helpers/global_helpers.dart';
 import 'package:cargo/presentation/screens/incoming_out_going.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 
 part 'incoming_out_going_event.dart';
 part 'incoming_out_going_state.dart';
 
-class IncomingOutGoingBloc extends Bloc<IncomingOutGoingEvent, IncomingOutGoingState> {
-  IncomingOutGoingBloc() : super(IncomingOutGoingInitial()) {
+const _postLimit = 10;
+
+class IncomingOutGoingBloc
+    extends Bloc<IncomingOutGoingEvent, IncomingOutGoingState> {
+  final http.Client httpClient;
+
+  IncomingOutGoingBloc({required this.httpClient})
+      : super(const IncomingOutGoingState()) {
     on<IncomingOutGoingEvent>((event, emit) {
       // TODO: implement event handler
     });
+  }
+
+  Future<void> _onIncomingOutGoingEvent(IncomingOutGoingEvent event,
+      Emitter<IncomingOutGoingState> emitter) async {
+    if (state.hasReachedMax) return;
+
+    try {
+      if (state.status == IncomingOutGoingStatus.initial) {
+        final incoming_out_going =
+            await _fetchincoming_out_going(page: state.page);
+        // ignore: invalid_use_of_visible_for_testing_member
+        return emit(
+          state.copyWith(
+            status: IncomingOutGoingStatus.success,
+            incoming_out_going: incoming_out_going,
+            hasReachedMax: incoming_out_going.length < _postLimit,
+          ),
+        );
+      }
+    } catch (e) {
+      return;
+    }
+  }
+
+  Future<List<IncomingOutGoing>> _fetchincoming_out_going({int? page}) async {
+    try {
+      final response = await httpClient.get(
+        getServerRoute(
+          route: '/api/v1/incoming_out_going',
+        ),
+        headers: <String, String>{
+          'Authorization':
+              'Bearer 1|2bcCa0xSXyODRPkS4AhEZSFSmr4OkmGVr9jv6Zw02881823b',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final body = json.decode(response.body)["data"] as List;
+
+        // return body.map((e) {
+        //   return IncomingOutGoing.fromMap(e);
+        // }).toList();
+      }
+      return [];
+    } catch (e) {
+      return [];
+    }
   }
 }
