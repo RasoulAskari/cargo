b5794712a711a878a66ce26808b2b3c2266823f8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 19:46:00 +0500

    IncomingOutGoingItem

---
+++ lib/presentation/widgets/incoming_out_going_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class IncomingOutGoingItem extends StatefulWidget {
+  const IncomingOutGoingItem({super.key});
+
+  @override
+  State<IncomingOutGoingItem> createState() => _IncomingOutGoingItemState();
+}
+
+class _IncomingOutGoingItemState extends State<IncomingOutGoingItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
