b5d4990cfa8a3c2647079cf2dd1811c3dc83ccb1
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-21 17:37:33 +0500

    IncomingOutGoing constrctor

--- lib/logic/incoming_out_going/model/incoming_out_going.dart
+++ lib/logic/incoming_out_going/model/incoming_out_going.dart
@@ -1,9 +1,13 @@
+import 'package:cargo/logic/login/user_model.dart';
 import 'package:equatable/equatable.dart';
 
 class IncomingOutGoing extends Equatable {
   final String name;
   final String type;
   final double amount;
+  final UserModel user;
+
+  IncomingOutGoing(this.name, this.type, this.amount, this.user);
 
   @override
   // TODO: implement props
