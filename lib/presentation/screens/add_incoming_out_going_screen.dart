a13b4fb209aba1a2765dc0dff6715d4ccc27e8e1
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-28 18:10:38 +0500

    CDatePicker add incoming

--- lib/presentation/screens/add_incoming_out_going_screen.dart
+++ lib/presentation/screens/add_incoming_out_going_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cargo/presentation/widgets/form/c_date_picker.dart';
 import 'package:flutter/material.dart';
 
 class AddIncomingOutGoingScreen extends StatefulWidget {
@@ -9,29 +10,39 @@ class AddIncomingOutGoingScreen extends StatefulWidget {
 }
 
 class _AddIncomingOutGoingScreenState extends State<AddIncomingOutGoingScreen> {
+  DateTime? _date;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: InkWell(
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-          child: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            child: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+          title: const Text(
+            "Add Incoming Out Going",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
-          "Add Incoming Out Going",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        body: Column(
+          children: [
+            CDatePicker(setValue: (value) {
+              setState(() {
+                _date = value;
+              });
+            })
+          ],
+        ));
   }
 }
