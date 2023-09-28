1e115574abb642da4af44f9e6c37919f6f3ca2df
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-28 17:40:59 +0500

    AddIncomingOutGoingScreen

--- lib/presentation/screens/add_incoming_out_going_screen.dart
+++ lib/presentation/screens/add_incoming_out_going_screen.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class AddIncomingOutGoingScreen extends StatefulWidget {
+  const AddIncomingOutGoingScreen({super.key});
+
+  @override
+  State<AddIncomingOutGoingScreen> createState() =>
+      _AddIncomingOutGoingScreenState();
+}
+
+class _AddIncomingOutGoingScreenState extends State<AddIncomingOutGoingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
