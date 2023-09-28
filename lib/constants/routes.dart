1f873d49654646008e68634890437ec5ccb2243e
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-28 17:43:17 +0500

    inconingOutGoingScreen

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,4 +1,5 @@
 const String home = '/home';
 const String addEmployee = '/addEmployee';
 const String employeeScreen = '/employeeScreen';
+const String inconingOutGoingScreen = '/inconingOutGoingScreen';
 const String addIncomingOutGoing = '/addIncomingOutGoing';
