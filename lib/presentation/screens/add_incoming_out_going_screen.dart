bec013eb70cd6bbb838d3c57ecdc3e8d3cf43b23
RasoulAskari <rasoul.merzai123@gmail.com>
2023-09-30 10:45:06 +0500

    TextField for amount

--- lib/presentation/screens/add_incoming_out_going_screen.dart
+++ lib/presentation/screens/add_incoming_out_going_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cargo/presentation/widgets/form/c_date_picker.dart';
 import 'package:cargo/presentation/widgets/form/c_drop_down.dart';
+import 'package:cargo/presentation/widgets/form/c_text_field.dart';
 import 'package:flutter/material.dart';
 
 class AddIncomingOutGoingScreen extends StatefulWidget {
@@ -16,49 +17,51 @@ class _AddIncomingOutGoingScreenState extends State<AddIncomingOutGoingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: InkWell(
-            onTap: () {
-              Navigator.of(context).pop();
-            },
-            child: const Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Theme.of(context).primaryColor,
-          title: const Text(
-            "Add Incoming Out Going",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-          child: Column(
-            children: [
-              CDatePicker(
-                setValue: (value) {
-                  setState(() {
-                    _date = value;
-                  });
-                },
-                hintText: "Register Date",
-              ),
-              const SizedBox(height: 15),
-              CDropdown(
-                hintText: "Type",
-                setValue: (value) {},
-                items: const [
-                  {'value': 'incoming', 'label': 'Incoming'},
-                  {'value': 'outgoing', 'label': 'Outgoing'},
-                ],
-              )
-            ],
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Add Incoming Out Going",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
           ),
-        ));
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        child: Column(
+          children: [
+            CDatePicker(
+              setValue: (value) {
+                setState(() {
+                  _date = value;
+                });
+              },
+              hintText: "Register Date",
+            ),
+            const SizedBox(height: 15),
+            CDropdown(
+              hintText: "Type",
+              setValue: (value) {},
+              items: const [
+                {'value': 'incoming', 'label': 'Incoming'},
+                {'value': 'outgoing', 'label': 'Outgoing'},
+              ],
+            ),
+            CTextField(value: "", setValue: (value) {})
+          ],
+        ),
+      ),
+    );
   }
 }
