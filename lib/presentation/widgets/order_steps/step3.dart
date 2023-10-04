51048fd3bc8823529ddf49b574b91ea30321425c
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-04 11:05:36 +0500

    Step3

--- lib/presentation/widgets/order_steps/step3.dart
+++ lib/presentation/widgets/order_steps/step3.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class Step3 extends StatefulWidget {
+  final Function next;
+  final Function prev;
+  const Step3({super.key, required this.next, required this.prev});
+
+  @override
+  State<Step3> createState() => _Step3State();
+}
+
+class _Step3State extends State<Step3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Step3");
+  }
+}
