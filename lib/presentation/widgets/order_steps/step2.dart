bd41432214edf742e61e4a2258d231fe64cc0e47
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-04 11:03:50 +0500

    Step 2

--- lib/presentation/widgets/order_steps/step2.dart
+++ lib/presentation/widgets/order_steps/step2.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class Step2 extends StatefulWidget {
+  final Function next;
+  final Function prev;
+  const Step2({super.key, required this.next, required this.prev});
+
+  @override
+  State<Step2> createState() => _Step2State();
+}
+
+class _Step2State extends State<Step2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
