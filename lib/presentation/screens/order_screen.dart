650feaf6febec4f243481186c12b34f002af0a7d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-04 09:12:58 +0500

    order Screen scaffold

--- lib/presentation/screens/order_screen.dart
+++ lib/presentation/screens/order_screen.dart
@@ -10,6 +10,34 @@ class OrderScreen extends StatefulWidget {
 class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(addEmployee);
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ))
+        ],
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Orders Screen",
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
