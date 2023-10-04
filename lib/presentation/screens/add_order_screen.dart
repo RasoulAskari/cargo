7feea5f26415cc1c18dd8f66d21b8d0aab882162
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-04 09:24:35 +0500

    AddOrderScreen

--- lib/presentation/screens/add_order_screen.dart
+++ lib/presentation/screens/add_order_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AddOrderScreen extends StatefulWidget {
+  const AddOrderScreen({super.key});
+
+  @override
+  State<AddOrderScreen> createState() => _AddOrderScreenState();
+}
+
+class _AddOrderScreenState extends State<AddOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
