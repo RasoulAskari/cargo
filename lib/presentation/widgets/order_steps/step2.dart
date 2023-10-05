bc46a7861e2a93d0bf56d436e94a08269d664700
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-05 16:54:08 +0500

    Step fill

--- lib/presentation/widgets/order_steps/step2.dart
+++ lib/presentation/widgets/order_steps/step2.dart
@@ -1,3 +1,7 @@
+import 'package:cargo/logic/form_models/phone_no.dart';
+import 'package:cargo/presentation/widgets/form/c_date_picker.dart';
+import 'package:cargo/presentation/widgets/form/c_phone_field.dart';
+import 'package:cargo/presentation/widgets/form/c_text_field.dart';
 import 'package:flutter/material.dart';
 
 class Step2 extends StatefulWidget {
@@ -10,8 +14,40 @@ class Step2 extends StatefulWidget {
 }
 
 class _Step2State extends State<Step2> {
+  PhoneNo phoneNo = const PhoneNo.pure();
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Step2");
+    return Column(
+      children: [
+        CTextField(value: "", hintText: "Group Number", setValue: () {}),
+        const SizedBox(height: 30),
+        CDatePicker(
+          setValue: (value) {},
+          hintText: "Date",
+        ),
+        const SizedBox(height: 30),
+        CTextField(value: "", hintText: "Name", setValue: () {}),
+        const SizedBox(height: 30),
+        CTextField(value: "", hintText: "Father Name", setValue: () {}),
+        const SizedBox(height: 30),
+        CTextField(value: "", hintText: "Grand Father Name", setValue: () {}),
+        const SizedBox(height: 30),
+        CTextField(value: "", hintText: "ID Card Number", setValue: () {}),
+        const SizedBox(height: 30),
+        CPhoneField(
+            hintText: "Phone Num",
+            setValue: (value) {},
+            setValid: () {},
+            value: phoneNo.value),
+        const SizedBox(height: 30),
+        CTextField(
+          hintText: "Price",
+          value: "",
+          setValue: (value) {},
+          textInputType: TextInputType.number,
+        )
+      ],
+    );
   }
 }
