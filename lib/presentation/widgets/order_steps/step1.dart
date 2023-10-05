3751304482d6494c1bbd2a3d391eb23d0d413dc5
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-05 11:17:47 +0500

    Phone Number

--- lib/presentation/widgets/order_steps/step1.dart
+++ lib/presentation/widgets/order_steps/step1.dart
@@ -1,3 +1,5 @@
+import 'package:cargo/logic/form_models/phone_no.dart';
+import 'package:cargo/presentation/widgets/form/c_phone_field.dart';
 import 'package:cargo/presentation/widgets/form/c_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +13,8 @@ class Step1 extends StatefulWidget {
 }
 
 class _Step1State extends State<Step1> {
+  PhoneNo phoneNo = const PhoneNo.pure();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,6 +26,14 @@ class _Step1State extends State<Step1> {
           CTextField(value: "", hintText: "Father Name", setValue: () {}),
           const SizedBox(height: 30),
           CTextField(value: "", hintText: "Grand Father Name", setValue: () {}),
+          const SizedBox(height: 30),
+          CTextField(value: "", hintText: "ID Card Number", setValue: () {}),
+          const SizedBox(height: 30),
+          CPhoneField(
+              hintText: "Phone Num",
+              setValue: (value) {},
+              setValid: () {},
+              value: phoneNo.value)
         ],
       ),
     );
