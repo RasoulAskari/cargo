290fc5db731b7ae5415996f04d9ea1be16a79901
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-06 18:03:41 +0500

    OrderItem

---
+++ lib/presentation/widgets/order_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OrderItem extends StatelessWidget {
+  const OrderItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
