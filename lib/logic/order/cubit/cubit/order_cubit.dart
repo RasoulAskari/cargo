bfbdb90e1778c80f94a94773427c904bdf5e18e6
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-06 09:24:58 +0500

    remove extra code

--- lib/logic/order/cubit/cubit/order_cubit.dart
+++ lib/logic/order/cubit/cubit/order_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/form_models/c_string.dart';
-import 'package:cargo/logic/form_models/full_name.dart';
 import 'package:cargo/logic/form_models/models.dart';
 import 'package:cargo/presentation/widgets/amount.dart';
 import 'package:equatable/equatable.dart';
