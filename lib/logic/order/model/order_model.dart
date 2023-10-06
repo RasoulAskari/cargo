7ececc318e460786948e71ebadd7dcffc1019166
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-06 17:08:35 +0500

    OrderModel fields

--- lib/logic/order/model/order_model.dart
+++ lib/logic/order/model/order_model.dart
@@ -1,6 +1,37 @@
 import 'package:equatable/equatable.dart';
 
 class OrderModel extends Equatable {
+  final int id;
+  final int cardId;
+  final String customerName;
+  final int groupNumber;
+  final String fatherName;
+  final String grandFatherName;
+  final String tazkiraId;
+  final String customerPhone;
+  final String receiverName;
+  final String receiverPhone;
+  final String country;
+  final String city;
+  final String address;
+  final String delivaryType;
+
+  const OrderModel(
+      {required this.id,
+      required this.cardId,
+      required this.customerName,
+      required this.groupNumber,
+      required this.fatherName,
+      required this.grandFatherName,
+      required this.tazkiraId,
+      required this.customerPhone,
+      required this.receiverName,
+      required this.receiverPhone,
+      required this.country,
+      required this.city,
+      required this.address,
+      required this.delivaryType});
+
   @override
   // TODO: implement props
   List<Object?> get props => [];
