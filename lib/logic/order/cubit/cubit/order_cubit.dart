b4ffffafcb171941469ef4cc76557274c4184df6
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-07 16:02:24 +0500

    cPhoneNumberChange

--- lib/logic/order/cubit/cubit/order_cubit.dart
+++ lib/logic/order/cubit/cubit/order_cubit.dart
@@ -2,9 +2,11 @@ import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/form_models/c_string.dart';
 import 'package:cargo/logic/form_models/models.dart';
 import 'package:cargo/presentation/widgets/amount.dart';
+import 'package:cargo/utils/phone_number.dart';
 import 'package:equatable/equatable.dart';
 import 'package:cargo/logic/form_models/first_name.dart';
 import 'package:formz/formz.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 part 'order_state.dart';
 
@@ -45,10 +47,10 @@ class OrderCubit extends Cubit<OrderState> {
     );
   }
 
-  void cPhoneNumberChange(String value) {
-    final customerIdCard = CString.dirty(value);
+  void cPhoneNumberChange(value) {
+    final cPhoneNumber = PhoneNo.dirty(value);
     emit(
-      state.copyWith(customerIdCard: customerIdCard),
+      state.copyWith(customerPhoneNo: cPhoneNumber),
     );
   }
 }
