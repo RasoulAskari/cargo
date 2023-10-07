4d6c11c4c84ab4dbfacd0a75f836431e2e724825
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-07 17:32:21 +0500

    remove extra code

--- lib/logic/order/cubit/cubit/order_cubit.dart
+++ lib/logic/order/cubit/cubit/order_cubit.dart
@@ -2,11 +2,9 @@ import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/form_models/c_string.dart';
 import 'package:cargo/logic/form_models/models.dart';
 import 'package:cargo/presentation/widgets/amount.dart';
-import 'package:cargo/utils/phone_number.dart';
 import 'package:equatable/equatable.dart';
 import 'package:cargo/logic/form_models/first_name.dart';
 import 'package:formz/formz.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 part 'order_state.dart';
 
