1988ed116175b53ced4291be306dcadf58a87215
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-09 16:28:52 +0500

    OrderItem

---
+++ lib/logic/order/model/order_item.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+
+class OrderItem extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
