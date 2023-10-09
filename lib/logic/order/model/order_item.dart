4374c4d4560dde94278026d5a3de03c826367909
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-09 16:33:01 +0500

    OrderItem json serilazation

--- lib/logic/order/model/order_item.dart
+++ lib/logic/order/model/order_item.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class OrderItem extends Equatable {
@@ -15,4 +18,27 @@ class OrderItem extends Equatable {
   @override
   // TODO: implement props
   List<Object?> get props => [name, type, count, weight];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'type': type,
+      'count': count,
+      'weight': weight,
+    };
+  }
+
+  factory OrderItem.fromMap(Map<String, dynamic> map) {
+    return OrderItem(
+      name: map['name'] as String,
+      type: map['type'] as String,
+      count: map['count'] as int,
+      weight: map['weight'] as double,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory OrderItem.fromJson(String source) =>
+      OrderItem.fromMap(json.decode(source) as Map<String, dynamic>);
 }
