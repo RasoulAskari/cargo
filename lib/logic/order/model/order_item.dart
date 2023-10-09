df082d810018a6377c3c1b9e05b36d6e7b90eb7a
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-09 16:31:03 +0500

    OrderItem params

--- lib/logic/order/model/order_item.dart
+++ lib/logic/order/model/order_item.dart
@@ -1,6 +1,17 @@
 import 'package:equatable/equatable.dart';
 
 class OrderItem extends Equatable {
+  final String name;
+  final String type;
+  final int count;
+  final double weight;
+
+  OrderItem(
+      {required this.name,
+      required this.type,
+      required this.count,
+      required this.weight});
+
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
