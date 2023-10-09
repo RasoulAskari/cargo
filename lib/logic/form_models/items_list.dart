309e07610020c344c0a874e9b94176aa5845438c
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-09 17:53:01 +0500

    change map to MyOrderItme in ItemsList

--- lib/logic/form_models/items_list.dart
+++ lib/logic/form_models/items_list.dart
@@ -1,11 +1,12 @@
+import 'package:cargo/logic/order/model/order_item.dart';
 import 'package:formz/formz.dart';
 
-class ItemsList extends FormzInput<List<Map>?, List<String>> {
+class ItemsList extends FormzInput<List<MyOrderItme>?, List<String>> {
   const ItemsList.pure() : super.pure(null);
-  const ItemsList.dirty(List<Map> value) : super.dirty(value);
+  const ItemsList.dirty(List<MyOrderItme> value) : super.dirty(value);
 
   @override
-  List<String>? validator(List<Map>? value) {
+  List<String>? validator(List<MyOrderItme>? value) {
     if (value == null || value.isEmpty) {
       return ['The list cannot be empty.'];
     }
