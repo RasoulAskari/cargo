13ec8bdf8b1af928b4e0c1804df5c596272b00ba
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-10 11:02:43 +0500

    OrderItemState fields

--- lib/logic/order/cubit/cubit/order_item_state.dart
+++ lib/logic/order/cubit/cubit/order_item_state.dart
@@ -1,11 +1,19 @@
 part of 'order_item_cubit.dart';
 
 sealed class OrderItemState extends Equatable {
+  final CString name;
+  final CString type;
+  final Number count;
+  final Amount weight;
 
-  const OrderItemState();
+  const OrderItemState(
+      {required this.name,
+      required this.type,
+      required this.count,
+      required this.weight});
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [name, type, count, weight];
 }
 
 final class OrderItemInitial extends OrderItemState {}
