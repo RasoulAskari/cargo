4ce64f73353f0398db8bae882b6e3cb73cf6e332
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 10:40:30 +0500

    SalaryBloc

--- lib/logic/salary/bloc/salary_bloc.dart
+++ lib/logic/salary/bloc/salary_bloc.dart
@@ -1,14 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/salary/model/salary_model.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 
 part 'salary_event.dart';
 part 'salary_state.dart';
 
 class SalaryBloc extends Bloc<SalaryEvent, SalaryState> {
-  SalaryBloc() : super(SalaryInitial()) {
-    on<SalaryEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+  final http.Client httpClient;
+
+  SalaryBloc({required this.httpClient}) : super(const SalaryState()) {}
 }
