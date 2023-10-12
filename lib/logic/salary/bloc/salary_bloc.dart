12797dd4312e85b9fe4f82bf9729abbe9c104f5e
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 19:26:37 +0500

    _onFetchSalaryOutGoingEvent

--- lib/logic/salary/bloc/salary_bloc.dart
+++ lib/logic/salary/bloc/salary_bloc.dart
@@ -19,5 +19,43 @@ class SalaryBloc extends Bloc<SalaryEvent, SalaryState> {
       FetchSalaryEvent event, Emitter<SalaryState> emitter) async {}
 
   Future<void> _onAddSalaryOutGoingEvent(
-      AddSalaryEvent event, Emitter<SalaryState> emitter) async {}
+      AddSalaryEvent event, Emitter<SalaryState> emitter) async {
+    print("idididi");
+    if (state.hasReachedMax) return;
+    SalaryModel salary = event.salary;
+    final data = {
+      'employee_id': salary.employee.id,
+      'created_at': salary.date.toString(),
+      'employee': {
+        'id': salary.employee.id,
+        "salary": salary.employee.salary,
+        'first_name': salary.employee.firstName,
+        'last_name': salary.employee.lastName
+      },
+      'amount': salary.salaryAmount,
+      "paid": salary.payAmount
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://localhost:8000/api/v1/salary-payments'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization':
+              'Bearer 1|2bcCa0xSXyODRPkS4AhEZSFSmr4OkmGVr9jv6Zw02881823b',
+        },
+        body: jsonEncode(data),
+      );
+
+      emitter(
+        state.copyWith(
+            // salary_out_going: List.of(state.salary_out_going)
+            //   ..insert(0, event.salaryOutGoing),
+            ),
+      );
+    } catch (e) {
+      print(e.toString());
+      return;
+    }
+  }
 }
