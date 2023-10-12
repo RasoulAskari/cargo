85b260f7af95f8ae1b8aeed02e83e758542022a4
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 09:04:26 +0500

    salry screen appBar

--- lib/presentation/screens/salary_screen.dart
+++ lib/presentation/screens/salary_screen.dart
@@ -10,6 +10,34 @@ class SalaryScreen extends StatefulWidget {
 class _SalaryScreenState extends State<SalaryScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("addEmployee");
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 30,
+              ))
+        ],
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Employee Screen",
+          style: TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
