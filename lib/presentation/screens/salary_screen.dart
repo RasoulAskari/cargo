26971abc26c5de42ac94ba2692063f9c062d4ff8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 09:03:10 +0500

    SalaryScreen

---
+++ lib/presentation/screens/salary_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalaryScreen extends StatefulWidget {
+  const SalaryScreen({super.key});
+
+  @override
+  State<SalaryScreen> createState() => _SalaryScreenState();
+}
+
+class _SalaryScreenState extends State<SalaryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
