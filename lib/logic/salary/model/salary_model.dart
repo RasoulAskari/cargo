7d71f6a599f743cbda0168f0abc9e4bc21c61f49
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 10:26:31 +0500

    Salary model property

--- lib/logic/salary/model/salary_model.dart
+++ lib/logic/salary/model/salary_model.dart
@@ -1,6 +1,20 @@
+import 'package:cargo/logic/emloyee/model/employee_model.dart';
 import 'package:equatable/equatable.dart';
 
 class SalaryModel extends Equatable {
+  final EmployeeModel employee;
+  final double salaryAmount;
+  final String date;
+  final double payAmount;
+  final double remainAmount;
+
+  SalaryModel(
+      {required this.employee,
+      required this.salaryAmount,
+      required this.date,
+      required this.payAmount,
+      required this.remainAmount});
+
   @override
   // TODO: implement props
   List<Object?> get props => throw UnimplementedError();
