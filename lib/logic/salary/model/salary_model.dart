c2c4fd9614b6d41eb79c4c012b207f7dbbedbcd1
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 10:28:55 +0500

    SalaryModel jsonseralization

--- lib/logic/salary/model/salary_model.dart
+++ lib/logic/salary/model/salary_model.dart
@@ -1,5 +1,9 @@
-import 'package:cargo/logic/emloyee/model/employee_model.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
+
+import 'package:cargo/logic/emloyee/model/employee_model.dart';
 
 class SalaryModel extends Equatable {
   final EmployeeModel employee;
@@ -24,4 +28,29 @@ class SalaryModel extends Equatable {
         payAmount,
         remainAmount,
       ];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'employee': employee.toMap(),
+      'salaryAmount': salaryAmount,
+      'date': date,
+      'payAmount': payAmount,
+      'remainAmount': remainAmount,
+    };
+  }
+
+  factory SalaryModel.fromMap(Map<String, dynamic> map) {
+    return SalaryModel(
+      employee: EmployeeModel.fromMap(map['employee'] as Map<String, dynamic>),
+      salaryAmount: map['salaryAmount'] as double,
+      date: map['date'] as String,
+      payAmount: map['payAmount'] as double,
+      remainAmount: map['remainAmount'] as double,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SalaryModel.fromJson(String source) =>
+      SalaryModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
