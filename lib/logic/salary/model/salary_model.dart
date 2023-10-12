43fb34c40349386aa97563aa9201f23d042c9664
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-12 10:23:43 +0500

    SalaryModel

---
+++ lib/logic/salary/model/salary_model.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class SalaryModel extends Equatable{
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+}
