077966b606bfada3c643ae43251fb77a27633b65
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 16:54:00 +0500

    remove c_text_field warning

--- lib/presentation/widgets/form/c_text_field.dart
+++ lib/presentation/widgets/form/c_text_field.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-
 class CTextField extends StatefulWidget {
   final String value;
   final Function setValue;
