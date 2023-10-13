276db6155021d5cc8ef03d0b06b7d6d903bdc015
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 17:07:32 +0500

    rmeove incoming screen

--- lib/presentation/screens/incoming_out_going_screen.dart
+++ lib/presentation/screens/incoming_out_going_screen.dart
@@ -2,6 +2,7 @@ import 'package:cargo/constants/routes.dart';
 import 'package:cargo/logic/incoming_out_going/bloc/bloc/incoming_out_going_bloc.dart';
 import 'package:cargo/presentation/widgets/incoming_out_going_item.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class IncomingOutGoingScreen extends StatefulWidget {
