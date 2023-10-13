0e60340f74c732304447cb5eaae469d1d02265dd
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 12:30:57 +0500

    SalaryItem

---
+++ lib/presentation/widgets/salary_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalaryItem extends StatefulWidget {
+  const SalaryItem({super.key});
+
+  @override
+  State<SalaryItem> createState() => _SalaryItemState();
+}
+
+class _SalaryItemState extends State<SalaryItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
