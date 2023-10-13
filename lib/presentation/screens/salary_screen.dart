4a5405c93452c2afc125663ffd2225f98f634df6
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 12:04:48 +0500

    salary screen with blocBuider

--- lib/presentation/screens/salary_screen.dart
+++ lib/presentation/screens/salary_screen.dart
@@ -1,5 +1,8 @@
 import 'package:cargo/constants/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../logic/salary/bloc/salary_bloc.dart';
 
 class SalaryScreen extends StatefulWidget {
   const SalaryScreen({super.key});
@@ -12,33 +15,37 @@ class _SalaryScreenState extends State<SalaryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(addSalaryScreen);
+                },
+                icon: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 30,
+                ))
+          ],
+          leading: IconButton(
               onPressed: () {
-                Navigator.of(context).pushNamed(addSalaryScreen);
+                Navigator.of(context).pop();
               },
               icon: const Icon(
-                Icons.add,
+                Icons.arrow_back_ios,
                 color: Colors.white,
-                size: 30,
-              ))
-        ],
-        leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(
-              Icons.arrow_back_ios,
-              color: Colors.white,
-            )),
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
-          "Salary Screen",
-          style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+              )),
+          backgroundColor: Theme.of(context).primaryColor,
+          title: const Text(
+            "Salary Screen",
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-    );
+        body: BlocBuilder<SalaryBloc, SalaryState>(
+          builder: (context, state) {
+            return Container();
+          },
+        ));
   }
 }
