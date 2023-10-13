7511bd594c4ac9cd3f916a1ca7ce54b9eb61ad98
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 17:18:24 +0500

    remove test_helper waring

--- lib/utils/test/test_helper.dart
+++ lib/utils/test/test_helper.dart
@@ -1,6 +1,9 @@
 class TestHelper {
+  // ignore: constant_identifier_names
   static const String TextInputKeyValue = 'intl_text_input_key';
+  // ignore: constant_identifier_names
   static const String DropdownButtonKeyValue = 'intl_dropdown_key';
+  // ignore: constant_identifier_names
   static const String CountrySearchInputKeyValue = 'intl_search_input_key';
   static String Function(String? isoCode) countryItemKeyValue =
       (String? isoCode) => 'intl_country_${isoCode!.toUpperCase()}_key';
