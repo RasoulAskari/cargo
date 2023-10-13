f80f153ca60dcccfc076baf3c7f0d96122057e0d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-13 17:01:06 +0500

    remove incoming_out_going_state

--- lib/logic/incoming_out_going/bloc/bloc/incoming_out_going_state.dart
+++ lib/logic/incoming_out_going/bloc/bloc/incoming_out_going_state.dart
@@ -5,18 +5,21 @@ enum IncomingOutGoingStatus { initial, success, failure }
 class IncomingOutGoingState extends Equatable {
   const IncomingOutGoingState({
     this.status = IncomingOutGoingStatus.initial,
+    // ignore: non_constant_identifier_names
     this.incoming_out_going = const <IncomingOutGoing>[],
     this.hasReachedMax = false,
     this.page = 0,
   });
 
   final IncomingOutGoingStatus status;
+  // ignore: non_constant_identifier_names
   final List<IncomingOutGoing> incoming_out_going;
   final int page;
   final bool hasReachedMax;
 
   IncomingOutGoingState copyWith({
     IncomingOutGoingStatus? status,
+    // ignore: non_constant_identifier_names
     List<IncomingOutGoing>? incoming_out_going,
     bool? hasReachedMax,
     int? page,
