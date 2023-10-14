079ee4c6b84da24323b09b923290f2e054f92c89
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-14 18:02:03 +0500

    step2 final params

--- lib/presentation/widgets/exchange_money_steps/step2.dart
+++ lib/presentation/widgets/exchange_money_steps/step2.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Step2 extends StatelessWidget {
-  const Step2({super.key});
+  final Function prev;
+  final Function next;
+  const Step2({super.key, required this.prev, required this.next});
 
   @override
   Widget build(BuildContext context) {
