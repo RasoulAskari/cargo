8106bd33f7fbab81d3313d95719d41d40c939523
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-14 19:30:54 +0500

    ExchangeMoneyState constructor

--- lib/logic/exchange_money/cubit/cubit/exchange_money_state.dart
+++ lib/logic/exchange_money/cubit/cubit/exchange_money_state.dart
@@ -12,18 +12,17 @@ sealed class ExchangeMoneyState extends Equatable {
   final FirstName senderName;
   final CString province;
 
-  const ExchangeMoneyState({
-    required this.amount,
-    required this.currency,
-    required this.date,
-    required this.exchnageId,
-    required this.phoneNumber,
-    required this.receiverIdNo,
-    required this.receiverName,
-    required this.receiverFathername,
-    required this.senderName,
-    required this.province,
-  });
+  const ExchangeMoneyState(
+      {this.amount = const Amount.pure(),
+      this.currency = const CString.pure(),
+      this.date = const MyDate.pure(),
+      this.exchnageId = const CString.pure(),
+      this.phoneNumber = const PhoneNo.pure(),
+      this.receiverIdNo = const CString.pure(),
+      this.receiverName = const FirstName.pure(),
+      this.receiverFathername = const FirstName.pure(),
+      this.senderName = const FirstName.pure(),
+      this.province = const CString.pure()});
 
   @override
   List<Object> get props => [];
