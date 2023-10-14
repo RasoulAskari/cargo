178387c2a558aa39e847086547611cff9caa26b1
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-14 17:29:40 +0500

    AddExchangeMoneyScreen

---
+++ lib/presentation/screens/add_exchange_money_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddExchangeMoneyScreen extends StatelessWidget {
+  const AddExchangeMoneyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
