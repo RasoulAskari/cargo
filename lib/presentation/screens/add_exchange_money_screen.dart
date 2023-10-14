cc7ecbd61a06250d5239a4b30c9349867ce26087
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-14 17:51:47 +0500

    import http

--- lib/presentation/screens/add_exchange_money_screen.dart
+++ lib/presentation/screens/add_exchange_money_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddExchangeMoneyScreen extends StatelessWidget {
   const AddExchangeMoneyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Add Exchange money screen",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
