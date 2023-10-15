cee3821ccc4403c2203193939ffcbbc3aeba9af9
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-15 10:29:37 +0500

    senderNameChange

--- lib/logic/exchange_money/cubit/cubit/exchange_money_cubit.dart
+++ lib/logic/exchange_money/cubit/cubit/exchange_money_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/form_models/amount.dart';
 import 'package:cargo/logic/form_models/c_string.dart';
@@ -9,4 +11,11 @@ part 'exchange_money_state.dart';
 
 class ExchangeMoneyCubit extends Cubit<ExchangeMoneyState> {
   ExchangeMoneyCubit() : super(ExchangeMoneyInitial());
+
+  void senderNameChange(String name) {
+    final sName = FirstName.dirty(name);
+    emit(state.copyWith(
+      senderName: sName,
+    ));
+  }
 }
