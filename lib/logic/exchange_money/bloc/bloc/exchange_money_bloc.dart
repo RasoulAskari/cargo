45f32380cadaecb3f4ead721f24c77dc28254c54
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-15 17:25:28 +0500

    ExchangeMoneyState

--- lib/logic/exchange_money/bloc/bloc/exchange_money_bloc.dart
+++ lib/logic/exchange_money/bloc/bloc/exchange_money_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:cargo/logic/exchange_money/model/exchange_money_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'exchange_money_event.dart';
--- lib/logic/exchange_money/bloc/bloc/exchange_money_state.dart
+++ lib/logic/exchange_money/bloc/bloc/exchange_money_state.dart
@@ -1,10 +1,39 @@
 part of 'exchange_money_bloc.dart';
 
-sealed class ExchangeMoneyState extends Equatable {
-  const ExchangeMoneyState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum ExchangeMoneyStatus { initial, success, failure }
 
-final class ExchangeMoneyInitial extends ExchangeMoneyState {}
+class ExchangeMoneyState extends Equatable {
+  const ExchangeMoneyState({
+    this.status = ExchangeMoneyStatus.initial,
+    this.exchangeMoneys = const <ExchnageMoneyModel>[],
+    this.hasReachedMax = false,
+    this.page = 0,
+  });
+
+  final ExchangeMoneyStatus status;
+  final List<ExchnageMoneyModel> exchangeMoneys;
+  final int page;
+  final bool hasReachedMax;
+
+  ExchangeMoneyState copyWith({
+    ExchangeMoneyStatus? status,
+    List<ExchnageMoneyModel>? exchangeMoneys,
+    bool? hasReachedMax,
+    int? page,
+  }) {
+    return ExchangeMoneyState(
+      status: status ?? this.status,
+      exchangeMoneys: exchangeMoneys ?? this.exchangeMoneys,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      page: page ?? this.page,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''ExchangeMoneyState { status: $status, hasReachedMax: $hasReachedMax, exchangeMoneys: ${exchangeMoneys.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, exchangeMoneys, hasReachedMax, page];
+}
