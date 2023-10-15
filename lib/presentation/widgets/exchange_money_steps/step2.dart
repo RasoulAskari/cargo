f619d5849da612039e2a74a94d3d60aaac9505a8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-15 11:23:16 +0500

    import step2 dep

--- lib/presentation/widgets/exchange_money_steps/step2.dart
+++ lib/presentation/widgets/exchange_money_steps/step2.dart
@@ -1,4 +1,8 @@
+import 'package:cargo/logic/order/cubit/cubit/order_cubit.dart';
+import 'package:cargo/presentation/widgets/form/c_text_field.dart';
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Step2 extends StatelessWidget {
   final Function prev;
