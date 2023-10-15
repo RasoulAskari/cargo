8b0fc93ddc3956cdc6b676cccd93d43c67e9c6ca
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-15 19:17:24 +0500

    ExchangeMoneyItem

---
+++ lib/presentation/screens/exchange_money_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExchangeMoneyItem extends StatefulWidget {
+  const ExchangeMoneyItem({super.key});
+
+  @override
+  State<ExchangeMoneyItem> createState() => _ExchangeMoneyItemState();
+}
+
+class _ExchangeMoneyItemState extends State<ExchangeMoneyItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
