d8cef6382c09bc9a98220040f1f65de8cbc581ee
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-16 10:24:09 +0500

    login model json seraliztion

--- lib/logic/login/login_model.dart
+++ lib/logic/login/login_model.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class LoginModel extends Equatable {
@@ -13,4 +16,27 @@ class LoginModel extends Equatable {
   });
   @override
   List<Object?> get props => throw UnimplementedError();
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'token': token,
+      'date': date,
+    };
+  }
+
+  factory LoginModel.fromMap(Map<String, dynamic> map) {
+    return LoginModel(
+      email: map['email'] as String,
+      password: map['password'] as String,
+      token: map['token'] as String,
+      date: map['date'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory LoginModel.fromJson(String source) =>
+      LoginModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
