83ef066a211aa1b1a4de93616fb039e4ddf094e4
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-16 18:18:19 +0500

    remove warning

--- lib/logic/exchange_money/bloc/bloc/exchange_money_bloc.dart
+++ lib/logic/exchange_money/bloc/bloc/exchange_money_bloc.dart
@@ -43,6 +43,15 @@ class ExchangeMoneyBloc extends Bloc<ExchangeMoneyEvent, ExchangeMoneyState> {
         'province': exchangeMoney.province,
         'receiver_id_no': exchangeMoney.receiverIdNo,
       };
+
+      await http.put(
+        Uri.parse('http://localhost:8000/api/v1/exhange-money'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode(data),
+      );
     } catch (e) {
       return;
     }
