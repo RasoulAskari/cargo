1216b35fcf50ca0e84d0cd4020e13c6e9c9af807
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-16 10:23:25 +0500

    add token and date to login model

--- lib/logic/login/login_model.dart
+++ lib/logic/login/login_model.dart
@@ -3,8 +3,11 @@ import 'package:equatable/equatable.dart';
 class LoginModel extends Equatable {
   final String email;
   final String password;
-
+  final String token;
+  final String date;
   const LoginModel({
+    required this.token,
+    required this.date,
     required this.email,
     required this.password,
   });
