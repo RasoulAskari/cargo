cacc436c48f099eb5fcc9a7cc304cb99ce7a0fc7
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 18:46:40 +0500

    RoleModel copyWith

--- lib/logic/user/model/role_model.dart
+++ lib/logic/user/model/role_model.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
 class RoleModel extends Equatable {
@@ -6,12 +7,27 @@ class RoleModel extends Equatable {
   final List<String> action;
   final List<String> allowRole;
 
-  const RoleModel(
-      {required this.systemId,
-      required this.systemName,
-      required this.action,
-      required this.allowRole});
+  const RoleModel({
+    required this.systemId,
+    required this.systemName,
+    required this.action,
+    required this.allowRole,
+  });
 
   @override
   List<Object?> get props => throw UnimplementedError();
+
+  RoleModel copyWith({
+    String? systemId,
+    String? systemName,
+    List<String>? action,
+    List<String>? allowRole,
+  }) {
+    return RoleModel(
+      systemId: systemId ?? this.systemId,
+      systemName: systemName ?? this.systemName,
+      action: action ?? this.action,
+      allowRole: allowRole ?? this.allowRole,
+    );
+  }
 }
