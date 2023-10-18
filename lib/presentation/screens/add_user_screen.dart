2328ff5b004fda6cbf4d4f7d0539d6fdf3d21af3
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 16:49:04 +0500

    add user state ful

--- lib/presentation/screens/add_user_screen.dart
+++ lib/presentation/screens/add_user_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AddUserScreen extends StatelessWidget {
+class AddUserScreen extends StatefulWidget {
   const AddUserScreen({super.key});
 
+  @override
+  State<AddUserScreen> createState() => _AddUserScreenState();
+}
+
+class _AddUserScreenState extends State<AddUserScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
