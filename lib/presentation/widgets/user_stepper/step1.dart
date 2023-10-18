bd71a6dd9794bc6edc9b01231ffb5ca195d6c1f5
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 17:24:34 +0500

    step colun

--- lib/presentation/widgets/user_stepper/step1.dart
+++ lib/presentation/widgets/user_stepper/step1.dart
@@ -1,3 +1,6 @@
+import 'package:cargo/logic/user/cubit/cubit/user_cubit.dart';
+import 'package:cargo/presentation/widgets/form/c_text_field.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
 class Step1 extends StatelessWidget {
@@ -8,6 +11,24 @@ class Step1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+      child: Column(
+        children: [
+          BlocBuilder<UserCubit, UserState>(
+            builder: (context, state) {
+              return CTextField(
+                hintText: "Sender Name",
+                value: state.name.value,
+                setValue: (value) {
+                  // context.read<UserCubit>().senderNameChange(value);
+                },
+              );
+            },
+          ),
+          const SizedBox(height: 50),
+        ],
+      ),
+    );
   }
 }
