f23860f4d37a88a0c098217119df7891244ae2a2
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 16:28:26 +0500

    AddUserScreen

---
+++ lib/presentation/screens/add_user_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddUserScreen extends StatelessWidget {
+  const AddUserScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
