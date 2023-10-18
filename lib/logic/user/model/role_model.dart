95af271a85c9d95e05f773a931a7a1c62146226b
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 18:48:51 +0500

    RoleModel toMap, FromJson

--- lib/logic/user/model/role_model.dart
+++ lib/logic/user/model/role_model.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class RoleModel extends Equatable {
@@ -30,4 +32,27 @@ class RoleModel extends Equatable {
       allowRole: allowRole ?? this.allowRole,
     );
   }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'systemId': systemId,
+      'systemName': systemName,
+      'action': action,
+      'allowRole': allowRole,
+    };
+  }
+
+  factory RoleModel.fromMap(Map<String, dynamic> map) {
+    return RoleModel(
+      systemId: map['systemId'] as String,
+      systemName: map['systemName'] as String,
+      action: List<String>.from((map['action'] as List<String>)),
+      allowRole: List<String>.from((map['allowRole'] as List<String>)),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory RoleModel.fromJson(String source) =>
+      RoleModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
