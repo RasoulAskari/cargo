6b3f82a2e6b733875e49a3e06c55e0370e7ec869
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 17:48:00 +0500

    passsword step1

--- lib/presentation/widgets/user_stepper/step1.dart
+++ lib/presentation/widgets/user_stepper/step1.dart
@@ -40,6 +40,17 @@ class Step1 extends StatelessWidget {
             },
           ),
           const SizedBox(height: 50),
+          BlocBuilder<UserCubit, UserState>(
+            builder: (context, state) {
+              return CPasswordField(
+                value: state.password.value,
+                setValue: (value) {
+                  // context.read<UserCubit>().senderNameChange(value);
+                },
+              );
+            },
+          ),
+          const SizedBox(height: 50),
         ],
       ),
     );
