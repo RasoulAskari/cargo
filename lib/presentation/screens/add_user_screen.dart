c0e45facce3338fe1d51ff4a5ce0c2a2abaa5889
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 16:31:08 +0500

    adduser appbar

--- lib/presentation/screens/add_user_screen.dart
+++ lib/presentation/screens/add_user_screen.dart
@@ -5,6 +5,27 @@ class AddUserScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Add User",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
