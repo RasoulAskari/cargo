58789c45a221ac0cafe0cf5636fe172b2421a9a8
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 16:59:18 +0500

    CStepper add user

--- lib/presentation/screens/add_user_screen.dart
+++ lib/presentation/screens/add_user_screen.dart
@@ -2,6 +2,7 @@ import 'package:cargo/presentation/widgets/user_stepper/step2.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../../utils/stepper/c_stepper.dart';
 import '../widgets/user_stepper/step.dart';
 
 class AddUserScreen extends StatefulWidget {
@@ -55,26 +56,18 @@ class _AddUserScreenState extends State<AddUserScreen> {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        leading: InkWell(
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-          child: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Theme.of(context).primaryColor,
-        title: const Text(
-          "Add User",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: CStepper(
+            prev: () {
+              prev(steps);
+            },
+            loading: loading,
+            stepAmount: 3,
+            steps: steps,
+            step: step,
+            next: () {
+              next(steps);
+            },
+            onSubmit: () async {}));
   }
 }
