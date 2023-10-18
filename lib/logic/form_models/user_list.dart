1cdf0aa0ac496577d89f8fd42a483c6fd7af647e
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 18:59:59 +0500

    UserList

---
+++ lib/logic/form_models/user_list.dart
@@ -0,0 +1,18 @@
+import 'package:cargo/logic/login/user_model.dart';
+import 'package:cargo/logic/order/model/my_order_item.dart';
+// ignore: depend_on_referenced_packages
+import 'package:formz/formz.dart';
+
+class UserList extends FormzInput<List<UserModel>?, List<String>> {
+  const UserList.pure() : super.pure(null);
+  const UserList.dirty(List<UserModel> value) : super.dirty(value);
+
+  @override
+  List<String>? validator(List<UserModel>? value) {
+    if (value == null || value.isEmpty) {
+      return ['The list cannot be empty.'];
+    }
+    // Add more custom validation logic as needed.
+    return null;
+  }
+}
