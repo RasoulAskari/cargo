b5a77758ae43faf30e4b16cece9e92a8e6141fef
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-18 17:06:28 +0500

    user_cubit

---
+++ lib/logic/user/cubit/cubit/user_state.dart
@@ -0,0 +1,10 @@
+part of 'user_cubit.dart';
+
+sealed class UserState extends Equatable {
+  const UserState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class UserInitial extends UserState {}
