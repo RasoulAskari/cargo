d78fe2968bc7f8927fb6109aa8d79f345a7d07ac
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-19 10:38:42 +0500

    state.premissions.value?.forEach

--- lib/presentation/widgets/user_stepper/step2.dart
+++ lib/presentation/widgets/user_stepper/step2.dart
@@ -1,4 +1,6 @@
+import 'package:cargo/logic/user/cubit/cubit/user_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Step2 extends StatefulWidget {
   final Function next;
@@ -14,108 +16,20 @@ class _Step2State extends State<Step2> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
-      child: Column(
-        children: [
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CDatePicker(
-                value: state.date.value != ""
-                    ? DateTime.parse(state.date.value)
-                    : DateTime.now(),
-                setValue: (value) {
-                  context.read<OrderCubit>().dateChange(value.toString());
-                },
-                hintText: "Date",
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CTextField(
-                value: state.customerName.value,
-                hintText: "Name",
-                setValue: (value) {
-                  context.read<OrderCubit>().cNameChange(value);
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CTextField(
-                value: state.customerFathername.value,
-                hintText: "Fathername",
-                setValue: (value) {
-                  context.read<OrderCubit>().cFathernameChange(value);
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CTextField(
-                value: state.customerGrandFathername.value,
-                hintText: "Grand Fathername",
-                setValue: (value) {
-                  context.read<OrderCubit>().cGrandFathernameChange(value);
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CTextField(
-                value: state.customerIdCard.value,
-                hintText: "ID Card Number",
-                setValue: (value) {
-                  context.read<OrderCubit>().cTazkiraIdChange(value);
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CPhoneField(
-                hintText: "Phone Num",
-                setValue: (value) {
-                  context.read<OrderCubit>().cPhoneNumberChange(value);
-                },
-                setValid: (bool? isValidPassed) {
-                  setState(() {
-                    isValid = isValidPassed ?? false;
-                  });
-                },
-                value: state.customerPhoneNo.value,
-              );
-            },
-          ),
-          const SizedBox(height: 30),
-          BlocBuilder<OrderCubit, OrderState>(
-            builder: (context, state) {
-              return CTextField(
-                hintText: "Price",
-                value: state.pricePerKelo.value.toString(),
-                setValue: (value) {
-                  double val;
-                  if (value == "") {
-                    val = 1;
-                  } else {
-                    val = double.parse(value);
-                  }
+      child: BlocBuilder<UserCubit, UserState>(
+        builder: (context, state) {
+          List<String> role = [];
 
-                  context.read<OrderCubit>().pricePerKeloChange(val);
-                },
-                textInputType: TextInputType.number,
-              );
-            },
-          ),
-        ],
+          state.premissions.value?.forEach((element1) {
+            element1.action.forEach((element2) {
+              role.add(element2);
+            });
+          });
+
+          return Column(
+            children: [],
+          );
+        },
       ),
     );
   }
