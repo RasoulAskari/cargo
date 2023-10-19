3554079c533879354b24af8380263a6adecd61eb
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-19 18:46:26 +0500

    remove user_list warning

--- lib/logic/form_models/user_list.dart
+++ lib/logic/form_models/user_list.dart
@@ -1,4 +1,3 @@
-import 'package:cargo/logic/login/user_model.dart';
 import 'package:cargo/logic/user/model/role_model.dart';
 // ignore: depend_on_referenced_packages
 import 'package:formz/formz.dart';
