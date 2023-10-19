10faa215e3138f44e9565e6fc33561903286f68a
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-19 16:54:53 +0500

    widget.value

--- lib/presentation/widgets/form/check_box.dart
+++ lib/presentation/widgets/form/check_box.dart
@@ -29,7 +29,7 @@ class _ActionCheckBoxState extends State<ActionCheckBox> {
             });
           },
         ),
-        Text('Check this box'),
+        Text(widget.value),
       ],
     );
   }
