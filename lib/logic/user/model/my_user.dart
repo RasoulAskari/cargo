06ce88820629ca9e685c28228145e32c97e95d9f
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-21 18:47:14 +0500

    MyUser varibles

--- lib/logic/user/model/my_user.dart
+++ lib/logic/user/model/my_user.dart
@@ -1,6 +1,22 @@
 import 'package:equatable/equatable.dart';
 
 class MyUser extends Equatable {
+  final String name;
+  final String email;
+  final String role;
+  final String password;
+  final String confirmPassword;
+  final List<String> permissions;
+
+  MyUser({
+    required this.name,
+    required this.email,
+    required this.role,
+    required this.password,
+    required this.confirmPassword,
+    required this.permissions,
+  });
+
   @override
   List<Object?> get props => throw UnimplementedError();
 }
