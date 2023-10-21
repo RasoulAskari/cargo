ce886fcb1e7a7438563f71aa2884ca0b4ebf63e9
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-21 18:48:57 +0500

    MyUser.fromMap

--- lib/logic/user/model/my_user.dart
+++ lib/logic/user/model/my_user.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class MyUser extends Equatable {
@@ -19,4 +22,33 @@ class MyUser extends Equatable {
 
   @override
   List<Object?> get props => throw UnimplementedError();
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'role': role,
+      'password': password,
+      'confirmPassword': confirmPassword,
+      'permissions': permissions,
+    };
+  }
+
+  factory MyUser.fromMap(Map<String, dynamic> map) {
+    return MyUser(
+      name: map['name'] as String,
+      email: map['email'] as String,
+      role: map['role'] as String,
+      password: map['password'] as String,
+      confirmPassword: map['confirmPassword'] as String,
+      permissions: List<String>.from(
+        (map['permissions'] as List<String>),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory MyUser.fromJson(String source) =>
+      MyUser.fromMap(json.decode(source) as Map<String, dynamic>);
 }
