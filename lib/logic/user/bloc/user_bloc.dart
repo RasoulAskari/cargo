b1103c31726671b227a942f600c1fde6ded02b1b
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-21 18:12:53 +0500

    my_user.dart

--- lib/logic/user/bloc/user_bloc.dart
+++ lib/logic/user/bloc/user_bloc.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 
 import 'package:bloc/bloc.dart';
+import 'package:cargo/logic/login/user_model.dart';
+import 'package:cargo/logic/user/model/role_model.dart';
 import 'package:equatable/equatable.dart';
 
 part 'user_event.dart';
--- lib/logic/user/bloc/user_state.dart
+++ lib/logic/user/bloc/user_state.dart
@@ -1,10 +1,39 @@
 part of 'user_bloc.dart';
 
-sealed class UserState extends Equatable {
-  const UserState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum EmployeeStatus { initial, success, failure }
 
-final class UserInitial extends UserState {}
+class UserState extends Equatable {
+  const UserState({
+    this.status = EmployeeStatus.initial,
+    this.employees = const <RoleModel>[],
+    this.hasReachedMax = false,
+    this.page = 0,
+  });
+
+  final EmployeeStatus status;
+  final List<RoleModel> employees;
+  final int page;
+  final bool hasReachedMax;
+
+  UserState copyWith({
+    EmployeeStatus? status,
+    List<RoleModel>? employees,
+    bool? hasReachedMax,
+    int? page,
+  }) {
+    return UserState(
+      status: status ?? this.status,
+      employees: employees ?? this.employees,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      page: page ?? this.page,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''employeestate { status: $status, hasReachedMax: $hasReachedMax, employees: ${employees.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, employees, hasReachedMax, page];
+}
