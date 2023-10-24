5b088b1e9e92f3145e731cbf86d9fb85557eb3b6
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-24 10:11:26 +0500

    localizationsDelegates

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:cargo/config/app_router.dart';
 import 'package:cargo/config/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CargoApp extends StatefulWidget {
   const CargoApp({super.key});
@@ -13,6 +15,12 @@ class _CargoAppState extends State<CargoApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       theme: AppThemes.appThemeData[AppTheme.lightTheme],
       debugShowCheckedModeBanner: false,
       onGenerateRoute: AppRouter.onGenerateRoute,
