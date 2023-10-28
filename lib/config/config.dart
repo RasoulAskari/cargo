1b17b2e01a225bfe7f7142c1d6d1c0516c276e83
RasoulAskari <rasoul.merzai123@gmail.com>
2023-10-28 09:50:15 +0500

    change route

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,2 +1,2 @@
-const String serverAddress = "http://127.0.0.1:8000";
-const String applicationLink = "http://127.0.0.1:8000";
+const String serverAddress = "http://10.0.2.2:8000";
+const String applicationLink = "http://10.0.2.2:8000";
