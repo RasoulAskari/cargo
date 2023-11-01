b0a88cfa7f56ebb283d1efd5db2c090f54923797
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-01 14:47:09 +0500

    errorText: name.isNotValid                     ? getCStringError(name.error, context)                     : null

--- lib/presentation/screens/add_incoming_out_going_screen.dart
+++ lib/presentation/screens/add_incoming_out_going_screen.dart
@@ -164,6 +164,9 @@ class _AddIncomingOutGoingScreenState extends State<AddIncomingOutGoingScreen> {
               ),
               const SizedBox(height: 15),
               CTextField(
+                errorText: name.isNotValid
+                    ? getCStringError(name.error, context)
+                    : null,
                 hintText: AppLocalizations.of(context)?.item_name,
                 value: name.value,
                 setValue: (value) {
