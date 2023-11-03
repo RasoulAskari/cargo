f47b1da1b1d2063570f28d719b44272073c4ad38
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-03 22:38:11 +0500

    EmployeeState constractor

--- lib/logic/emloyee/cubit/employee_cubit.dart
+++ lib/logic/emloyee/cubit/employee_cubit.dart
@@ -1,4 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:cargo/logic/form_models/address.dart';
+import 'package:cargo/logic/form_models/email.dart';
+import 'package:cargo/logic/form_models/first_name.dart';
+import 'package:cargo/logic/form_models/my_date.dart';
+import 'package:cargo/logic/form_models/phone_no.dart';
 import 'package:equatable/equatable.dart';
 
 part 'employee_state.dart';
--- lib/logic/emloyee/cubit/employee_state.dart
+++ lib/logic/emloyee/cubit/employee_state.dart
@@ -1,10 +1,25 @@
 part of 'employee_cubit.dart';
 
 class EmployeeState extends Equatable {
-  
-  
+  final FirstName firstname;
+  final FirstName lastname;
+  final PhoneNo phoneNo;
+  final Email email;
+  final Address currentAddress;
+  final Address premenentAddress;
+  final MyDate startDate;
+  final MyDate endDate;
 
-  const EmployeeState();
+  const EmployeeState({
+    this.firstname = const FirstName.pure(),
+    this.lastname = const FirstName.pure(),
+    this.phoneNo = const PhoneNo.pure(),
+    this.email = const Email.pure(),
+    this.currentAddress = const Address.pure(),
+    this.premenentAddress = const Address.pure(),
+    this.startDate = const MyDate.pure(),
+    this.endDate = const MyDate.pure(),
+  });
 
   @override
   List<Object> get props => [];
