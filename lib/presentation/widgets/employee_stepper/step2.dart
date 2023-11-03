c13a92d1584fe945e1d2b7354de59210d386a1a4
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-03 21:46:35 +0500

    step2 final params

--- lib/presentation/widgets/employee_stepper/step2.dart
+++ lib/presentation/widgets/employee_stepper/step2.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Step2 extends StatefulWidget {
-  const Step2({super.key});
+  final Function prev;
+  final Function next;
+  const Step2({super.key, required this.prev, required this.next});
 
   @override
   State<Step2> createState() => _Step2State();
