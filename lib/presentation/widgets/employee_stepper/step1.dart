1f25993f4e3bb8d9eda359ae8d67905ddbc4a64a
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-03 21:44:00 +0500

    Step1 employee

---
+++ lib/presentation/widgets/employee_stepper/step1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Step1 extends StatefulWidget {
+  const Step1({super.key});
+
+  @override
+  State<Step1> createState() => _Step1State();
+}
+
+class _Step1State extends State<Step1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
