f5df7f9270c700f9c646ca141eefbf14c42bcb4d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-13 17:31:10 +0500

    InputLabel(label: AppLocalizations.of(context)!.type),

--- lib/presentation/widgets/exchange_money_steps/step2.dart
+++ lib/presentation/widgets/exchange_money_steps/step2.dart
@@ -24,7 +24,7 @@ class _Step2State extends State<Step2> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
       child: Column(
         children: [
           BlocBuilder<ExchangeMoneyCubit, ExchangeMoneyState>(
