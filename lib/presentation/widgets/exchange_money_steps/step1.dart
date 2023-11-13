aa45013e29fb5dd02b66c8e942e2609ad1654e30
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-13 17:29:55 +0500

    InputLabel(label: AppLocalizations.of(context)!.exchange_money_id),

--- lib/presentation/widgets/exchange_money_steps/step1.dart
+++ lib/presentation/widgets/exchange_money_steps/step1.dart
@@ -79,7 +79,7 @@ class Step1 extends StatelessWidget {
               );
             },
           ),
-          const SizedBox(height: 50),
+          InputLabel(label: AppLocalizations.of(context)!.exchange_money_id),
           BlocBuilder<ExchangeMoneyCubit, ExchangeMoneyState>(
             builder: (context, state) {
               return CTextField(
