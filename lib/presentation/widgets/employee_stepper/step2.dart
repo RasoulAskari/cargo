6616ab35400a88466f838c202d65f51ec75262dc
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-13 16:46:23 +0500

    InputLabel(label: AppLocalizations.of(context)!.premenent_address),

--- lib/presentation/widgets/employee_stepper/step2.dart
+++ lib/presentation/widgets/employee_stepper/step2.dart
@@ -41,7 +41,7 @@ class _Step2State extends State<Step2> {
               );
             },
           ),
-          const SizedBox(height: 30),
+          InputLabel(label: AppLocalizations.of(context)!.premenent_address),
           BlocBuilder<EmployeeCubit, EmployeeState>(
             builder: (context, state) {
               return CTextField(
