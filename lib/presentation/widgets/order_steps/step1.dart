33d638c3fa7564b89051584c82bef10596c8d8f2
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-13 17:16:58 +0500

    InputLabel(label: AppLocalizations.of(context)!.grand_father_name),

--- lib/presentation/widgets/order_steps/step1.dart
+++ lib/presentation/widgets/order_steps/step1.dart
@@ -75,7 +75,7 @@ class _Step1State extends State<Step1> {
               );
             },
           ),
-          const SizedBox(height: 30),
+          InputLabel(label: AppLocalizations.of(context)!.grand_father_name),
           BlocBuilder<OrderCubit, OrderState>(
             builder: (context, state) {
               return CTextField(
