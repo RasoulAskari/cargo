8c9354583dd28a80df5b3b67e9e9a2f7916f661d
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-17 09:07:21 +0500

    type label padding
    undefined

--- lib/presentation/widgets/order_steps/step3.dart
+++ lib/presentation/widgets/order_steps/step3.dart
@@ -100,7 +100,10 @@ class _Step3State extends State<Step3> {
                 },
               ),
             ),
-            InputLabel(label: AppLocalizations.of(context)!.type),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: InputLabel(label: AppLocalizations.of(context)!.type),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: BlocBuilder<OrderItemCubit, OrderItemState>(
