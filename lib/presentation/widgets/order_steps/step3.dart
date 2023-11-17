7a119278d1eb36da02b3dc91bc4375b2e63d8449
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-17 09:08:24 +0500

    weight label padding

--- lib/presentation/widgets/order_steps/step3.dart
+++ lib/presentation/widgets/order_steps/step3.dart
@@ -152,7 +152,10 @@ class _Step3State extends State<Step3> {
                 },
               ),
             ),
-            InputLabel(label: AppLocalizations.of(context)!.weight),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: InputLabel(label: AppLocalizations.of(context)!.weight),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: BlocBuilder<OrderItemCubit, OrderItemState>(
