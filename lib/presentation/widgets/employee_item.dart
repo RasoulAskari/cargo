c520e7320583746447f7617be7f4bec7103ea498
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-20 17:48:15 +0500

    call show Detail employee

--- lib/presentation/widgets/employee_item.dart
+++ lib/presentation/widgets/employee_item.dart
@@ -110,98 +110,103 @@ class _EmployeeItemState extends State<EmployeeItem> {
 
     return Column(
       children: [
-        Container(
-          padding: const EdgeInsets.only(right: 15),
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+        InkWell(
+          onTap: () {
+            showDetail();
+          },
+          child: Container(
+            padding: const EdgeInsets.only(right: 15),
+            height: 100,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-          ),
-          margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-          child: ListTile(
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-            leading: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.black12, // Border color
-                  width: 3.0, // Border width
+            margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: ListTile(
+              contentPadding:
+                  const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+              leading: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.black12, // Border color
+                    width: 3.0, // Border width
+                  ),
+                ),
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://i.pravatar.cc/300?u=e52552f4-835d-4dbe-ba77-b076e659774d'))),
                 ),
               ),
-              child: Container(
-                height: 70,
-                width: 70,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                            'https://i.pravatar.cc/300?u=e52552f4-835d-4dbe-ba77-b076e659774d'))),
+              title: Container(
+                margin: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  "${widget.employee.firstName} ${widget.employee.lastName}",
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                      color: Color(0Xff303030)),
+                ),
               ),
-            ),
-            title: Container(
-              margin: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                "${widget.employee.firstName} ${widget.employee.lastName}",
+              subtitle: Text(
+                "${widget.employee.email}\n${widget.employee.phoneNumber}",
                 style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
-                    color: Color(0Xff303030)),
+                  color: Colors.black54,
+                  fontSize: 12,
+                ),
               ),
-            ),
-            subtitle: Text(
-              "${widget.employee.email}\n${widget.employee.phoneNumber}",
-              style: const TextStyle(
-                color: Colors.black54,
-                fontSize: 12,
-              ),
-            ),
-            trailing: SizedBox(
-              width: 110,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      checkPermissionAction(() {
-                        Navigator.of(context)
-                            .pushNamed(addEmployee, arguments: widget.employee);
-                      }, "employee_restore", context);
-                    },
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                            color: const Color(0x55ff711b),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          Icons.edit_document,
-                          color: Theme.of(context).primaryColor,
-                        )),
-                  ),
-                  const SizedBox(width: 3),
-                  GestureDetector(
-                    onTap: () {
-                      checkPermissionAction(() {
-                        onDelete();
-                      }, 'employee_delete', context);
-                    },
-                    child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 5),
-                        decoration: BoxDecoration(
-                            color: const Color(0x55ff711b),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Icon(
-                          Icons.delete_forever,
-                          color: Theme.of(context).primaryColor,
-                        )),
-                  ),
-                ],
+              trailing: SizedBox(
+                width: 110,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        checkPermissionAction(() {
+                          Navigator.of(context).pushNamed(addEmployee,
+                              arguments: widget.employee);
+                        }, "employee_restore", context);
+                      },
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
+                          decoration: BoxDecoration(
+                              color: const Color(0x55ff711b),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.edit_document,
+                            color: Theme.of(context).primaryColor,
+                          )),
+                    ),
+                    const SizedBox(width: 3),
+                    GestureDetector(
+                      onTap: () {
+                        checkPermissionAction(() {
+                          onDelete();
+                        }, 'employee_delete', context);
+                      },
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 5),
+                          decoration: BoxDecoration(
+                              color: const Color(0x55ff711b),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.delete_forever,
+                            color: Theme.of(context).primaryColor,
+                          )),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
