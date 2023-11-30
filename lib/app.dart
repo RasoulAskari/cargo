3f3d502ee934c352fcd1b307a27d4cb9cea06e31
RasoulAskari <rasoul.merzai123@gmail.com>
2023-11-30 16:32:46 +0500

    context.read<LanguageCubit>().changeLanguage(lang as String);

--- lib/app.dart
+++ lib/app.dart
@@ -20,6 +20,7 @@ class _CargoAppState extends State<CargoApp> {
   @override
   void initState() {
     final lang = _getLanguage();
+    context.read<LanguageCubit>().changeLanguage(lang as String);
     super.initState();
   }
 
