291322221c405eb3524af1cb05a6a3b1f9594090
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-01 00:02:01 +0500

    password inputlabel

--- lib/presentation/screens/Login_screen.dart
+++ lib/presentation/screens/Login_screen.dart
@@ -137,6 +137,12 @@ class _LoginScreenState extends State<LoginScreen> {
                                           height: 25,
                                         ),
                                         Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 16,
+                                          ),
+                                          child: InputLabel(label: "Password"),
+                                        ),
+                                        Padding(
                                           padding: const EdgeInsets.symmetric(
                                               horizontal: 16),
                                           child: CTextField(
