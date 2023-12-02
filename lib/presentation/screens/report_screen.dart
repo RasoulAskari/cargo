35ff2f05de0cc33d7064c9e1db462411350dbacb
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-02 16:20:30 +0500

    report screen body

--- lib/presentation/screens/report_screen.dart
+++ lib/presentation/screens/report_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cargo/config/localization.dart';
 import 'package:flutter/material.dart';
 
 class ReportScreen extends StatelessWidget {
@@ -5,6 +6,26 @@ class ReportScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          AppLocalizations.of(context)!.employee_screen,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
