8e545652d70b7fd5afa43f9d4bdc1d693ae8e298
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-03 18:49:13 +0500

    Car Screen body

--- lib/presentation/screens/car_screen.dart
+++ lib/presentation/screens/car_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cargo/config/localization.dart';
 import 'package:flutter/material.dart';
 
 class CarScreen extends StatelessWidget {
@@ -5,6 +6,26 @@ class CarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            )),
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text(
+          AppLocalizations.of(context)!.report_screen,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
