acaec2c5a5c5e2bdc49cd5fa9486d2e281bb24cf
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-03 18:46:06 +0500

    CarScreen

---
+++ lib/presentation/screens/car_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CarScreen extends StatelessWidget {
+  const CarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
