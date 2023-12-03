bca6acba15f04e10a0fac74efdc9bf7b87a5ad71
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-03 19:10:38 +0500

    car model

--- lib/logic/car/bloc/car_state.dart
+++ lib/logic/car/bloc/car_state.dart
@@ -1,10 +1,39 @@
 part of 'car_bloc.dart';
 
-sealed class CarState extends Equatable {
-  const CarState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum EmployeeStatus { initial, success, failure }
 
-final class CarInitial extends CarState {}
+class EmployeeState extends Equatable {
+  const EmployeeState({
+    this.status = EmployeeStatus.initial,
+    this.employees = const <EmployeeModel>[],
+    this.hasReachedMax = false,
+    this.page = 0,
+  });
+
+  final EmployeeStatus status;
+  final List<EmployeeModel> employees;
+  final int page;
+  final bool hasReachedMax;
+
+  EmployeeState copyWith({
+    EmployeeStatus? status,
+    List<EmployeeModel>? employees,
+    bool? hasReachedMax,
+    int? page,
+  }) {
+    return EmployeeState(
+      status: status ?? this.status,
+      employees: employees ?? this.employees,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      page: page ?? this.page,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''employeestate { status: $status, hasReachedMax: $hasReachedMax, employees: ${employees.length} }''';
+  }
+
+  @override
+  List<Object> get props => [status, employees, hasReachedMax, page];
+}
