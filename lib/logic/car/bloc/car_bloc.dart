6c7dd5ec67bd89715a350e5c9ee953c2bfcc1d46
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-03 19:14:25 +0500

    changess

--- lib/logic/car/bloc/car_bloc.dart
+++ lib/logic/car/bloc/car_bloc.dart
@@ -7,8 +7,6 @@ part 'car_state.dart';
 
 class CarBloc extends Bloc<CarEvent, CarState> {
   CarBloc() : super(const CarState()) {
-    on<CarEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<CarEvent>((event, emit) {});
   }
 }
