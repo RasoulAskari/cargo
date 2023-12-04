4ff4e72fd91536dc4a3ff752707c84a283181895
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-04 17:32:24 +0500

    change under score to camal case

--- lib/logic/car/model/car_model.dart
+++ lib/logic/car/model/car_model.dart
@@ -3,28 +3,29 @@ import 'package:equatable/equatable.dart';
 class CarModel extends Equatable {
   final int id;
   final int status;
-  final double payments_sum_amount;
-  final double extra_expense_sum_price;
-  final double car_expense_sum_price;
-  final double orders_count;
-  final double total_price;
-  final String start_date;
-  final String end_date;
+  final double paymentsSumAmount;
+  final double extraExpenseSumPrice;
+  final double carExpenseSumPrice;
+  final double ordersCount;
+  final double totalPrice;
+  final String startDate;
+  final String endDate;
   final double benefits;
   final double remainder;
 
-  const CarModel(
-      {required this.id,
-      required this.status,
-      required this.payments_sum_amount,
-      required this.extra_expense_sum_price,
-      required this.car_expense_sum_price,
-      required this.orders_count,
-      required this.total_price,
-      required this.start_date,
-      required this.end_date,
-      required this.benefits,
-      required this.remainder});
+  const CarModel({
+    required this.id,
+    required this.status,
+    required this.paymentsSumAmount,
+    required this.extraExpenseSumPrice,
+    required this.carExpenseSumPrice,
+    required this.ordersCount,
+    required this.totalPrice,
+    required this.startDate,
+    required this.endDate,
+    required this.benefits,
+    required this.remainder,
+  });
 
   @override
   List<Object?> get props => throw UnimplementedError();
