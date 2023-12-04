e373f37b11bd700cb48fffc5e255d65d4d80992b
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-04 16:52:27 +0500

    _fetchCars

--- lib/logic/car/bloc/car_bloc.dart
+++ lib/logic/car/bloc/car_bloc.dart
@@ -1,12 +1,43 @@
 import 'package:bloc/bloc.dart';
 import 'package:cargo/logic/car/model/car_model.dart';
+import 'package:cargo/logic/helpers/global_helpers.dart';
 import 'package:equatable/equatable.dart';
+import 'package:http/http.dart' as http;
 
 part 'car_event.dart';
 part 'car_state.dart';
 
+const _postLimit = 10;
+
 class CarBloc extends Bloc<CarEvent, CarState> {
-  CarBloc() : super(const CarState()) {
+  final http.Client httpClient;
+
+  CarBloc({required this.httpClient}) : super(const CarState()) {
     on<CarEvent>((event, emit) {});
+  }
+
+  Future<List<CarModel>> _fetchCars({int? page}) async {
+    final token = await getAuthToken();
+    try {
+      final response = await httpClient.get(
+        getServerRoute(
+          route: '/api/v1/orders',
+        ),
+        headers: <String, String>{
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        // final body = json.decode(response.body)["data"] as List;
+        // return body.map((e) {
+        //   return CarModel.fromMap(e);
+        // }).toList();
+      }
+
+      return [];
+    } catch (e) {
+      return [];
+    }
   }
 }
