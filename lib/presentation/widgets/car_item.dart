a16cd2f51f5b7eca42d01f14168dc135a63733c9
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-05 12:29:33 +0500

    final CarModel car;

--- lib/presentation/widgets/car_item.dart
+++ lib/presentation/widgets/car_item.dart
@@ -1,7 +1,9 @@
+import 'package:cargo/logic/car/model/car_model.dart';
 import 'package:flutter/material.dart';
 
 class CarItem extends StatelessWidget {
-  const CarItem({super.key});
+  final CarModel car;
+  const CarItem({super.key, required this.car});
 
   @override
   Widget build(BuildContext context) {
