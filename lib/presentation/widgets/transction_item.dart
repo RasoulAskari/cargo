71cf2515119b3a1f4fc44718241536cc32c607bf
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-10 16:50:44 +0500

    changes

--- lib/presentation/widgets/transction_item.dart
+++ lib/presentation/widgets/transction_item.dart
@@ -4,14 +4,29 @@ class TransctionItem extends StatelessWidget {
   final Icon icon;
   final String value;
   final String label;
-  const TransctionItem(
-      {super.key,
-      required this.icon,
-      required this.value,
-      required this.label});
+  const TransctionItem({
+    super.key,
+    required this.icon,
+    required this.value,
+    required this.label,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Container(
+          height: 40,
+          width: 40,
+          child: icon,
+        ),
+        Column(
+          children: [
+            Text(label),
+            Text(value),
+          ],
+        )
+      ],
+    );
   }
 }
