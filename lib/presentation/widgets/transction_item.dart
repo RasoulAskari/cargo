8434801ee63465765b4713e3b0ea701fad3c8fe2
RasoulAskari <rasoul.merzai123@gmail.com>
2023-12-10 16:44:26 +0500

    TransctionItem

---
+++ lib/presentation/widgets/transction_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransctionItem extends StatelessWidget {
+  const TransctionItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
